949354ee512c4ef39a0843fe09ed7757390dbb74
till <>
2003-12-18 19:41:22 +0000

    - minor tweaks

--- mkmezca.m
+++ mkmezca.m
@@ -1,4 +1,5 @@
 function mkmezca()
+mk('ecget.c')
 mk('ezcaGetControlLimits.c')
 mk('ezcaPut.c')
 mk('ezcaGet.c')
@@ -6,5 +7,5 @@ mk('ezcaGetNelem.c')
 %
 %
 function mk(feil)
-eval(['mex -cxx -DMATLAB_APP -I. -Iezca -I/afs/slac/g/spear/epics/base/include -I/afs/slac/g/spear/epics/base/include/os/Linux -L. ',feil,' -lmezcaglue'])
+eval(['mex -cxx -DMATLAB_APP -DmexErrMsgTxt=mexWarnMsgTxt -I. -Iezca -I/afs/slac/g/spear/3.14.2epics/base/include -I/afs/slac/g/spear/3.14.2epics/base/include/os/Linux -L. ',feil,' -lmezcaglue'])
 %-L/afs/slac/g/spear/epics/base/lib/linux-x86 -Llib/linux-x86 -lezca314 -lca -lCom
