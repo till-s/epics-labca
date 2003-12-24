d8aed534fcd5c79d799ba8c1c61173153eaa3d7c
till <>
2003-12-24 17:30:52 +0000

    - added ezcaSetSeverityWarnLevel

---
+++ matlab/lcaSetSeverityWarnLevel.c
@@ -0,0 +1,32 @@
+/* $Id: ezcaSetRetryCount.c,v 1.2 2003/12/23 23:06:56 strauman Exp $ */
+
+/* matlab wrapper for ezcaSetSeverityWarnLevel */
+
+/* Author: Till Straumann <strauman@slac.stanford.edu>, 2002-2003  */
+
+/* LICENSE: EPICS open license, see ../LICENSE file */
+
+#include "mglue.h"
+#include "multiEzca.h"
+
+#include <cadef.h>
+#include <ezca.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+	if ( 1 < nlhs ) {
+		MEXERRPRINTF("Need one output arg");
+		return;
+	}
+
+	if ( 1 != nrhs ) {
+		MEXERRPRINTF("Expected one rhs argument");
+		return;
+	}
+
+	if ( !mxIsNumeric(prhs[0]) || 1 != mxGetM(prhs[0]) || 1 != mxGetN(prhs[0]) ) {
+		MEXERRPRINTF("Need a single numeric argument");
+		return;
+	}
+	ezcaSeverityWarnLevel = (int)mxGetScalar(prhs[0]);
+}
