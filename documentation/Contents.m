1f59b6b82b87665c3af1d5ad39b941687d656f17
till <>
2004-01-02 22:04:52 +0000

    - marked unsupported ezca routines 'NOT IMPLEMENTED YET'

--- documentation/Contents.m
+++ documentation/Contents.m
@@ -4,12 +4,12 @@
 %   ezcaGet                   - read one or multiple EPICS PVs
 %   ezcaPut                   - write one or multiple EPICS PVs
 %   ezcaGetNelem              - retrieve max. number of elements of EPICS PVs
-%   ezcaGetStatus             - read status, severity and timestamp of EPICS PVs
+%   ezcaGetStatus             - NOT IMPLEMENTED YET (read status, severity and timestamp of EPICS PVs)
 %   ezcaGetControlLimits      - read control limits of EPICS PVs
 %   ezcaGetGraphicLimits      - read graphic limits of EPICS PVs
-%   ezcaGetPrecision          - read the display precision associated with EPICS PVs
-%   ezcaGetUnits              - read the engineering units string associated with EPICS PVs
+%   ezcaGetPrecision          - NOT IMPLEMENTED YET (read the display precision associated with EPICS PVs)
+%   ezcaGetUnits              - NOT IMPLEMENTED YET (read the engineering units string associated with EPICS PVs)
 %   ezcaGetRetryCount         - read/set the EZCA library retry count parameter
 %   ezcaGetTimeout            - read/set the EZCA library timeout parameter
-%   ezcaDebugOn               - toggle EZCA library debugging messages on/off
+%   ezcaDebugOn               - NOT IMPLEMENTED YET (toggle EZCA library debugging messages on/off)
 %   ezcaSetSeverityWarnLevel  - set the warning threshold used when reading EPICS 'VAL' PVs
