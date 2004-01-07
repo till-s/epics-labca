d2436ead7c1b09bdbcfcbc28d46d342b0d39c186
till <>
2004-01-07 01:19:53 +0000

    - renamed ezcaXxxx --> lcaXxxx

--- documentation/Contents.m
+++ documentation/Contents.m
@@ -1,15 +1,15 @@
 % EPICS Channel Access Interface
 % ==============================
 %
-%   ezcaGet                   - read one or multiple EPICS PVs
-%   ezcaPut                   - write one or multiple EPICS PVs
-%   ezcaGetNelem              - retrieve max. number of elements of EPICS PVs
-%   ezcaGetStatus             - NOT IMPLEMENTED YET (read status, severity and timestamp of EPICS PVs)
-%   ezcaGetControlLimits      - read control limits of EPICS PVs
-%   ezcaGetGraphicLimits      - read graphic limits of EPICS PVs
-%   ezcaGetPrecision          - NOT IMPLEMENTED YET (read the display precision associated with EPICS PVs)
-%   ezcaGetUnits              - NOT IMPLEMENTED YET (read the engineering units string associated with EPICS PVs)
-%   ezcaGetRetryCount         - read/set the EZCA library retry count parameter
-%   ezcaGetTimeout            - read/set the EZCA library timeout parameter
-%   ezcaDebugOn               - NOT IMPLEMENTED YET (toggle EZCA library debugging messages on/off)
-%   ezcaSetSeverityWarnLevel  - set the warning threshold used when reading EPICS 'VAL' PVs
+%   lcaGet                   - read one or multiple EPICS PVs
+%   lcaPut                   - write one or multiple EPICS PVs
+%   lcaGetNelem              - retrieve max. number of elements of EPICS PVs
+%   lcaGetStatus             - NOT IMPLEMENTED YET (read status, severity and timestamp of EPICS PVs)
+%   lcaGetControlLimits      - read control limits of EPICS PVs
+%   lcaGetGraphicLimits      - read graphic limits of EPICS PVs
+%   lcaGetPrecision          - NOT IMPLEMENTED YET (read the display precision associated with EPICS PVs)
+%   lcaGetUnits              - NOT IMPLEMENTED YET (read the engineering units string associated with EPICS PVs)
+%   lcaGetRetryCount         - read/set the EZCA library retry count parameter
+%   lcaGetTimeout            - read/set the EZCA library timeout parameter
+%   lcaDebugOn               - NOT IMPLEMENTED YET (toggle EZCA library debugging messages on/off)
+%   lcaSetSeverityWarnLevel  - set the warning threshold used when reading EPICS 'VAL' PVs
