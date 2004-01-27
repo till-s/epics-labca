5af5b9939f1b5db40fb3649362ab1a275924a9e8
till <>
2004-01-27 03:58:16 +0000

    - initialize/install CtrlC handling

--- glue/ini.cc
+++ glue/ini.cc
@@ -1,10 +1,10 @@
-/* $Id: ini.cc,v 1.5 2003/12/31 07:58:17 till Exp $ */
+/* $Id: ini.cc,v 1.6 2004/01/14 02:49:47 till Exp $ */
 
 /* xlabcaglue library initializer */
 
 #include <cadef.h>
 #include <ezca.h>
-#include <mex.h>
+#include <multiEzca.h>
 
 #define DEF_TIMEOUT 0.2
 #define DEF_RETRIES 20
@@ -12,12 +12,22 @@
 static int
 ezlibinit()
 {
+
 /* don't print to stderr because that
  * doesn't go to scilab's main window...
  */
 mexPrintf((char*)"Initializing labCA Release '$Name:  $'...\n");
 mexPrintf((char*)"Author: Till Straumann <strauman@slac.stanford.edu>\n");
-ezcaAutoErrorMessageOff();
+
+multi_ezca_ctrlC_initialize();
+
+ezcaAutoErrorMessageOff(); /* calls ezca init() */
+
+#if defined(WIN32) || defined(_WIN32)
+/* Install a callback to detect windoze activity (Ctrl-C) */
+ezcaPollCbInstall( multi_ezca_pollCb );
+#endif
+
 mexPrintf((char*)"Polling interval is %.2fs; max. timeout: %.2fs\n",
 				DEF_TIMEOUT, DEF_RETRIES*DEF_TIMEOUT);
 ezcaSetTimeout((float)DEF_TIMEOUT);
