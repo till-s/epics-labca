1ebe5df212b2d6d65d71260a9d19550bc65a7ed6
till <>
2004-01-28 05:47:40 +0000

    - callback installation is now done in WIN specific multi_ezca_ctrlC_initialize()

--- glue/ini.cc
+++ glue/ini.cc
@@ -1,4 +1,4 @@
-/* $Id: ini.cc,v 1.6 2004/01/14 02:49:47 till Exp $ */
+/* $Id: ini.cc,v 1.7 2004/01/27 03:58:16 till Exp $ */
 
 /* xlabcaglue library initializer */
 
@@ -22,11 +22,6 @@ mexPrintf((char*)"Author: Till Straumann <strauman@slac.stanford.edu>\n");
 multi_ezca_ctrlC_initialize();
 
 ezcaAutoErrorMessageOff(); /* calls ezca init() */
-
-#if defined(WIN32) || defined(_WIN32)
-/* Install a callback to detect windoze activity (Ctrl-C) */
-ezcaPollCbInstall( multi_ezca_pollCb );
-#endif
 
 mexPrintf((char*)"Polling interval is %.2fs; max. timeout: %.2fs\n",
 				DEF_TIMEOUT, DEF_RETRIES*DEF_TIMEOUT);
