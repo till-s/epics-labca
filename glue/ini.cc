f9e3069e762676a71246687192b3860c3550b050
strauman <>
2004-06-23 18:04:56 +0000

    - use linker options rather than 'dlopen' to prevent our library stack    from being unloaded

--- glue/ini.cc
+++ glue/ini.cc
@@ -1,4 +1,4 @@
-/* $Id: ini.cc,v 1.13 2004/03/31 00:32:45 till Exp $ */
+/* $Id: ini.cc,v 1.14 2004/06/23 01:22:52 till Exp $ */
 
 /* xlabcaglue library initializer */
 
@@ -6,7 +6,12 @@
 #include <cadef.h>
 #include <ezca.h>
 #include <multiEzcaCtrlC.h>
-#if !defined(WIN32) && !defined(_WIN32) && defined(MATLAB_APP)
+
+/* This is now done via the '-z nodelete' linker option */
+#if !defined(WIN32) && !defined(_WIN32) && defined(MATLAB_APP) && defined(USE_DLOPEN)
+/* matlab tries to unload mex files and dependent libraries when terminating - something
+ * we want to prevent as ezca/CA cannot be shutdown cleanly.
+ */
 #include <dlfcn.h>
 #endif
 
@@ -35,10 +40,10 @@ CtrlCStateRec saved;
 /* don't print to stderr because that
  * doesn't go to scilab's main window...
  */
-mexPrintf((char*)"Initializing labCA Release '$Name:  $'...\n");
+mexPrintf((char*)"Initializing labCA Release '$Name: labca_2_0_alpha $'...\n");
 mexPrintf((char*)"Author: Till Straumann <strauman@slac.stanford.edu>\n");
 
-#if !defined(WIN32) && !defined(_WIN32) && defined(MATLAB_APP)
+#if !defined(WIN32) && !defined(_WIN32) && defined(MATLAB_APP) && defined(USE_DLOPEN)
 /* matlab tries to unload mex files and dependent libraries when terminating.
  * CA is not really prepared for a clean shutdown, so we lock our library
  * stack into memory
