4d0e7ff13c8483ba8875f5cf36551ebd3212139e
till <>
2006-04-14 23:52:13 +0000

    - implemented 'shutdown' procedure which is needed to avoid    crashing the application (scilab or matlab) when exiting.    We call this from an 'atexit' handler since a destructor    method is executed too late (when CA already has been pulled down??).    The current approach seems to work but I don't really understand    why nor why doing it slightly different fails for one app    (but sometimes not the other, i.e., scilab crashes but matlab doesnt    etc.)

--- glue/ini.cc
+++ glue/ini.cc
@@ -1,4 +1,4 @@
-/* $Id: ini.cc,v 1.20 2006/04/11 02:18:29 till Exp $ */
+/* $Id: ini.cc,v 1.21 2006/04/12 02:15:06 strauman Exp $ */
 
 /* xlabcaglue library initializer */
 
@@ -13,34 +13,65 @@
 #include <epicsExit.h>
 #endif
 
-/* This is now done via the '-z nodelete' linker option */
+#if defined(WIN32) || defined(_WIN32)
+typedef int pid_t;
+#define getpid _getpid
+extern "C" int _getpid();
+#else
+#include <unistd.h>
+#endif
+
+#ifdef SCILAB_APP
+#include <signal.h>
+#endif
+
+#undef  DEBUG
+
 #define USE_DLOPEN
-#if !defined(WIN32) && !defined(_WIN32) && defined(MATLAB_APP) && defined(USE_DLOPEN)
+
+#if defined(WIN32) || defined(_WIN32) || BASE_IS_MIN_VERSION(3,14,7)
+#undef USE_DLOPEN
+#endif
+
+#if defined(USE_DLOPEN)
 /* matlab tries to unload mex files and dependent libraries when terminating - something
  * we want to prevent as ezca/CA cannot be shutdown cleanly.
  */
 #include <dlfcn.h>
 #endif
 
-#ifdef SCILAB_APP
-#include <signal.h>
+static	pid_t thepid;
+
+static void
+multiEzcaFinalizer()
+{
+	if ( thepid != getpid() ) {
+		/* DONT run this if a forked caRepeater exits */
+#ifdef DEBUG
+		mexPrintf("labca finalizer: PID mismatch\n");
+#endif
+		return;
+	}
+	multi_ezca_clear_channels(0,-1);
+	ezcaLock();
+#if (!defined(WIN32) && !defined(_WIN32)) || defined(MATLAB_APP)
+	/* ca_context_destroy() hangs (scilab/win32) :-( */
+	ca_context_destroy();
 #endif
 #if BASE_IS_MIN_VERSION(3,14,7)
-void do_epics_exit()
-{
 	epicsExit(0);
-}
 #endif
+#ifdef DEBUG
+	mexPrintf("Leaving labca finalizer\n");
+#endif
+}
+
 
 class multiEzcaInitializer {
 public:
 	multiEzcaInitializer();
-#if BASE_IS_MIN_VERSION(3,14,7)
-	~multiEzcaInitializer()
-		{ do_epics_exit(); }
-	;
-#endif
 };
+
 
 multiEzcaInitializer::
 multiEzcaInitializer()
@@ -60,7 +91,8 @@ CtrlCStateRec saved;
 mexPrintf((char*)"Initializing labCA Release '$Name:  $'...\n");
 mexPrintf((char*)"Author: Till Straumann <strauman@slac.stanford.edu>\n");
 
-#if !defined(WIN32) && !defined(_WIN32) && defined(MATLAB_APP) && defined(USE_DLOPEN) && !BASE_IS_MIN_VERSION(3,14,7)
+#if defined(USE_DLOPEN)
+#ifdef MATLAB_APP
 /* matlab tries to unload mex files and dependent libraries when terminating.
  * CA is not really prepared for a clean shutdown, so we lock our library
  * stack into memory
@@ -69,28 +101,28 @@ mexPrintf((char*)"Author: Till Straumann <strauman@slac.stanford.edu>\n");
 	mexPrintf((char*)"Locking library in memory failed: %s\n",dlerror());
   }
 #endif
+#ifdef SCILAB_APP
+  if ( !dlopen("libsezcaglue.so",RTLD_NOW) ) {
+	mexPrintf((char*)"Locking library in memory failed: %s\n",dlerror());
+  }
+#endif
+#endif
 
 multi_ezca_ctrlC_initialize();
 
 multi_ezca_ctrlC_prologue(&saved);
 ezcaAutoErrorMessageOff(); /* calls ezca init() */
 multi_ezca_ctrlC_epilogue(&saved);
-#if BASE_IS_MIN_VERSION(3,14,7)
-atexit(do_epics_exit);
-#endif
 
+/* On unix the class destructor is called too late; atexit seems to
+ * give a cleaner shutdown.
+ * Don't know how to do this on win32 -- there, the destructor seems
+ * to be good enough...
+ * Another problem on unix is the 'fork'ed caRepeater calling the
+ * finalizer...
+ */
+thepid = getpid();
+atexit(multiEzcaFinalizer);
 }
-
-#if 0 /* this doesn't work properly */
-multiEzcaInitializer::
-~multiEzcaInitializer()
-{
-multi_ezca_clear_channels(0,-1);
-ezcaLock();
-ca_context_destroy();
-mexPrintf("Leaving labca finalizer\n");
-}
-#endif
 
 static multiEzcaInitializer theini;
-
