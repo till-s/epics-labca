cdd6a4c985723f665dbc5a584efa6a7214713659
guest <>
2007-06-04 18:57:13 +0000

    - implemented lcaGetPrecision, lcaGetUnits

--- documentation/Contents.m
+++ documentation/Contents.m
@@ -9,8 +9,8 @@
 %   lcaGetStatus             - read status, severity and timestamp of EPICS PVs
 %   lcaGetControlLimits      - read control limits of EPICS PVs
 %   lcaGetGraphicLimits      - read graphic limits of EPICS PVs
-%   lcaGetPrecision          - NOT IMPLEMENTED YET (read the display precision associated with EPICS PVs)
-%   lcaGetUnits              - NOT IMPLEMENTED YET (read the engineering units string associated with EPICS PVs)
+%   lcaGetPrecision          - read the display precision associated with EPICS PVs
+%   lcaGetUnits              - read the engineering units string associated with EPICS PVs
 %   lcaGetRetryCount         - read/set the EZCA library retry count parameter
 %   lcaGetTimeout            - read/set the EZCA library timeout parameter
 %   lcaDebugOn               - NOT IMPLEMENTED YET (toggle EZCA library debugging messages on/off)
