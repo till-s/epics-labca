918ac0745907e57b080920f88d4b21c8dff9cf2d
jbellister-slac <jesseb@slac.stanford.edu>
2022-12-12 21:51:25 -0800

    Enable the ability to use lcaDebugOn and lcaDebugOff

--- documentation/Contents.m
+++ documentation/Contents.m
@@ -14,7 +14,7 @@
 %   lcaGetEnumStrings         - read the ENUM strings defined for a PV
 %   lcaGetRetryCount         - read/set the EZCA library retry count parameter
 %   lcaGetTimeout            - read/set the EZCA library timeout parameter
-%   lcaDebugOn               - NOT IMPLEMENTED YET (toggle EZCA library debugging messages on/off)
+%   lcaDebugOn               - toggle EZCA library debugging messages on/off
 %   lcaSetSeverityWarnLevel  - set the warning threshold used when reading EPICS 'VAL' PVs
 %   lcaClear                 - clear (destroy/cleanup) channels and associated monitors
 %   lcaSetMonitor            - monitor a channel
